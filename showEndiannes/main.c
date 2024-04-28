4e74eff812d900b3d2b95486536afa9d39062dec
AriTeil <118675580+AriTeilWork@users.noreply.github.com>
2024-04-28 20:04:00 +0300

    Update main.c

--- showEndiannes/main.c
+++ showEndiannes/main.c
@@ -1,23 +1,23 @@
+// this program will show the endianness of the computer
 #include <stdio.h>
 union myUnion {
 	int x;
 	char a[4];
 	float f;
+
 };
 
 int main(void) {
 	union myUnion m;
 	m.x = 0x0A0B0C0D;
-	printf("%d\n", 0123);
-	printf("%d\n", 0b1010011);
-	printf("%d\n", 0x53);
-	printf("%d\n", 'a');
-
-	if (m.a[0] == 0x0A) printf("big endian\n");
-	if (m.a[0] == 0x0D) printf("little endian\n");
+	if (m.a[0] == 0x0A) {
+		printf("Big-endian\n");
+	}
+	else {
+		printf("Little-endian\n");
+	}
 	m.f = 0.15625;
-	for (int i = 0; i < 4; i++)
-	{
+	for (int i = 0; i < 4; i++) {
 		printf("%02x", m.a[i]);
 	}
 	printf("\n");
