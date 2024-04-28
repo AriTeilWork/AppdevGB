14b02a8939a8906cefee7c2c4f53444f09008d9f
AriTeil <118675580+AriTeilWork@users.noreply.github.com>
2024-04-28 20:03:11 +0300

    Update screen.h

--- pascalTringle/screen.h
+++ pascalTringle/screen.h
@@ -1,6 +1,12 @@
 #pragma once
 enum COLORS{BLACK=30,RED,GREEN,YELLOW,BLUE,MAGENTA,CYAN,WHITE};
 
-void gotoXY(int row, int col);
-void resetColors(void);
+void setFGcolor(int color);
+
 void clearScreen(void);
+
+void gotoXY(int row, int col);
+
+void resetColor();
+
+void setBgColor(int color);
